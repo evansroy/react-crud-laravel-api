8744017cc97c4dbec23ed67cfc65c439a2623c0f
Evans <evansroysir@gmail.com>
2023-03-26 10:49:50 +0300

    Add axio dependency to help fetch data from  api

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -1,12 +1,47 @@
+import axios from 'axios';
 import React from 'react'
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
 
 function Student() {
 
+    const [loading, setLoading] = useState(true);
+    const [students, setStudents] = useState([]);
+
     useEffect(() => {
-        
-    })
+        axios.get(`http://127.0.0.1:8000/api/students`).then(res => {
+            console.log(res)
+            setStudents(res.data.students);
+            setLoading(false);
+        });
+    }, [])
+
+    if(loading){ 
+        return(
+           <Loading/>
+        )
+    }
+
+    var studentDetails = "";
+    studentDetails = students.map( (item,index) => {
+        return(
+
+            <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.firstname}</td>
+                <td>{item.lastname}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>
+                    <Link to="/" className='btn btn-success'>Edit</Link>
+                </td>
+                <td>
+                    <button className='btn btn-danger'>Delete</button>
+                </td>
+            </tr>
+        )
+    });
   return (
     <div className='container mt-5'>
       <div className='row'>
@@ -31,7 +66,7 @@ function Student() {
                             </tr>
                         </thead>
                         <tbody>
-
+                            {studentDetails}
                         </tbody>
                     </table>
                 </div>
