e785ed3c8a685586d46dd99fca9c59150249369c
Evans <evansroysir@gmail.com>
2023-03-26 06:42:09 +0300

    Add Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import { Link } from "react-router-dom";
+import React from 'react'
+import Navbar from "./components/Navbar";
 import MyRouter from "./router";
 
 function App() {
   return (
     <div>
-      <Link to= "/">Home</Link>
-      <Link to= "/about-us">About Us</Link>
-      <Link to= "/contact-us">Contact Us</Link>
-
+      <Navbar/>
       <MyRouter/>
     </div>
   );
